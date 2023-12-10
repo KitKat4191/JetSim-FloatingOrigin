aab36c5a19fc4be8f2898b22a8836dc6db24e9ad
KitKat <katjastoroygard@gmail.com>
2023-12-10 05:35:02 +0100

    Added readme link

---
+++ Editor/FO_ToolbarHelpURLs.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+using UnityEditor;
+
+public static class FO_ToolbarHelpURLs
+{
+    [MenuItem("KitKat/JetSim/Floating Origin/📝 README", priority = -10000)]
+    public static void OpenReadme()
+    {
+        Application.OpenURL("https://github.com/KitKat4191/JetSim-FloatingOrigin/blob/main/README.md");
+    }
+}
