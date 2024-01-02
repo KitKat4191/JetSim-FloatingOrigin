f3f316f1cf06b74851bf55d6a42f55e6046ce94d
KitKat <katjastoroygard@gmail.com>
2024-01-02 06:27:26 +0100

    Update FO_MenuItems.cs
    Made property public and renamed accordingly

--- Editor/FO_MenuItems.cs
+++ Editor/FO_MenuItems.cs
@@ -6,41 +6,41 @@ namespace KitKat.JetSim.FloatingOrigin.Editor
 {
     public static class FO_MenuItems
     {
-        private const string _FOLDER_PATH = "KitKat/JetSim/Floating Origin/";
+        public const string FOLDER_PATH = "KitKat/JetSim/Floating Origin/";
 
 
-        [MenuItem(_FOLDER_PATH + "📝 README", priority = -10000)]
+        [MenuItem(FOLDER_PATH + "📝 README", priority = -10000)]
         private static void OpenReadme() => Application.OpenURL("https://github.com/KitKat4191/JetSim-FloatingOrigin/blob/main/README.md");
 
         ////////////////////////
 
-        [MenuItem(_FOLDER_PATH + "Install", priority = 80)]
+        [MenuItem(FOLDER_PATH + "Install", priority = 80)]
         private static void Install() => FO_Installer.Install();
 
         ////////////////////////
 
-        [MenuItem(_FOLDER_PATH + "Execute All Automation", priority = 100)]
+        [MenuItem(FOLDER_PATH + "Execute All Automation", priority = 100)]
         private static void ExecuteAutomation() => FO_BuildValidator.RunOnBuild();
 
         ////////////////////////
         
-        [MenuItem(_FOLDER_PATH + "Add Station Notifiers", priority = 120)]
+        [MenuItem(FOLDER_PATH + "Add Station Notifiers", priority = 120)]
         private static void AddStationNotifiers() => FO_BuildValidator.SetUpStationNotifiers();
 
-        [MenuItem(_FOLDER_PATH + "Remove Station Notifiers", priority = 121)]
+        [MenuItem(FOLDER_PATH + "Remove Station Notifiers", priority = 121)]
         private static void RemoveStationNotifiers() => FO_BuildValidator.RemoveAllStationNotifiers();
 
         ////////////////////////
 
-        [MenuItem(_FOLDER_PATH + "Validate Particle Systems", priority = 140)]
+        [MenuItem(FOLDER_PATH + "Validate Particle Systems", priority = 140)]
         private static void ValidateParticleSystems() => FO_Installer.ValidateParticleSystems();
 
-        [MenuItem(_FOLDER_PATH + "Restore Simulation Spaces", priority = 141)]
+        [MenuItem(FOLDER_PATH + "Restore Simulation Spaces", priority = 141)]
         private static void RestoreParticleSimulationSpaces() => FO_BuildValidator.RestoreParticleSimulationSpaces();
 
         ////////////////////////
 
-        [MenuItem(_FOLDER_PATH + "Disable All Static Flags", priority = 400)]
+        [MenuItem(FOLDER_PATH + "Disable All Static Flags", priority = 400)]
         private static void DisableStaticFlags() => FO_BuildValidator.DisableStatics();
     }
 }
