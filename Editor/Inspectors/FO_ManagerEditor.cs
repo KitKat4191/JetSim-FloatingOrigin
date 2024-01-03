3ab9122dae8666abf53ebca8b3b8de4f3e969cf9
KitKat <katjastoroygard@gmail.com>
2024-01-03 18:07:14 +0100

    Reset shader global when exiting play-mode

--- Editor/Inspectors/FO_ManagerEditor.cs
+++ Editor/Inspectors/FO_ManagerEditor.cs
@@ -9,6 +9,19 @@ namespace KitKat.JetSim.FloatingOrigin.Editor
     [CustomEditor(typeof(FO_Manager))]
     public class FO_ManagerEditor : UnityEditor.Editor
     {
+        [InitializeOnLoadMethod]
+        private static void Hook()
+        {
+            EditorApplication.playModeStateChanged += OnPlayModeChanged;
+            Shader.SetGlobalVector("_Udon_FO_WorldOffset", Vector3.zero);
+        }
+
+        private static void OnPlayModeChanged(PlayModeStateChange state)
+        {
+            if (state != PlayModeStateChange.EnteredEditMode) return;
+            Shader.SetGlobalVector("_Udon_FO_WorldOffset", Vector3.zero);
+        }
+
         public override void OnInspectorGUI()
         {
             GUIStyle style = new GUIStyle(EditorStyles.helpBox);
